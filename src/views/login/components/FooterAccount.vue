e71919a87db440e622ba7ee0c0446b6b8f3ed297
vorn-dev-ni <Nightpp19@gmail.com>
2024-03-13 21:00:33 +0700

    fix: dialog animation

--- src/views/login/components/FooterAccount.vue
+++ src/views/login/components/FooterAccount.vue
@@ -184,4 +184,26 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+
+<style scoped>
+.dialog-enter-from,
+.dialog-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.dialog-enter-active {
+  transition: all 0.3s ease-in;
+}
+
+.dialog-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.dialog-enter-to,
+.dialog-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+</style>
+
